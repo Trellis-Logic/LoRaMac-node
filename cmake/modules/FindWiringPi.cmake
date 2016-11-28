6339d7d48eeb6c7cd9a730cf0622864b9f7f69d8
Rainer 'rei' Schuth <r.schuth@cuculus.net>
2016-11-28 10:42:40 +0100

    Add wiringpi include dirs to search path for wiringPi.h

--- cmake/modules/FindWiringPi.cmake
+++ cmake/modules/FindWiringPi.cmake
@@ -1,5 +1,6 @@
 find_library(WIRINGPI_LIBRARIES NAMES wiringPi)
 find_path(WIRINGPI_INCLUDE_DIRS NAMES wiringPi.h)
+include_directories(SYSTEM ${WIRINGPI_INCLUDE_DIRS})
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(wiringPi "Could not find WiringPi.  Either install it using instructions at http://wiringpi.com/download-and-install/ or specify a different BOARD_GPIO" WIRINGPI_LIBRARIES WIRINGPI_INCLUDE_DIRS)
